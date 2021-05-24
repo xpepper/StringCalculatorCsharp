02cc5f8650b4f79158d5e6038f638ff67baf0903
Pietro Di Bello <pierodibello@gmail.com>
2021-05-24 17:25:08 +0200

    extract test helper to avoid repetitions

--- StringCalculatorKata.Tests/StringCalculatorTest.cs
+++ StringCalculatorKata.Tests/StringCalculatorTest.cs
@@ -9,14 +9,19 @@ namespace StringCalculatorKata.Tests
         [Fact]
         public void Return_0_when_the_string_is_empty()
         {
-            StringCalculator.Add("").Should().Be(0);
+            CheckAdd("", 0);
         }
 
         [Fact]
         public void Return_the_value_of_the_number_when_the_string_contains_just_that_number()
         {
-            StringCalculator.Add("1").Should().Be(1);
-            StringCalculator.Add("2").Should().Be(2);
+            CheckAdd("1", 1);
+            CheckAdd("2", 2);
+        }
+
+        private static void CheckAdd(string stringOfNumbers, int expectedSum)
+        {
+            StringCalculator.Add(stringOfNumbers).Should().Be(expectedSum);
         }
     }
 }
