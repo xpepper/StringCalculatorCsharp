f70483dd1688db355db34cd97b812605f4551f22
Pietro Di Bello <pierodibello@gmail.com>
2021-05-24 17:16:36 +0200

    return 0 when the string of numbers is empty

---
+++ StringCalculatorKata.Tests/StringCalculatorTest.cs
@@ -0,0 +1,23 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace StringCalculatorKata.Tests
+{
+    public class StringCalculatorTest
+    {
+        [Fact]
+        public void Return_0_when_the_string_is_empty()
+        {
+            StringCalculator.Add("").Should().Be(0);
+        }
+    }
+
+    public class StringCalculator
+    {
+        public static int Add(string stringOfNumbers)
+        {
+            return 0;
+        }
+    }
+}
