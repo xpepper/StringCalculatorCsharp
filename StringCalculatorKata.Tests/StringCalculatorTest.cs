bb96c640e7716439635b1bcca22c951226145d50
Pietro Di Bello <pierodibello@gmail.com>
2021-05-25 12:19:59 +0200

    make class static

--- StringCalculatorKata.Tests/StringCalculatorTest.cs
+++ StringCalculatorKata.Tests/StringCalculatorTest.cs
@@ -9,21 +9,21 @@ namespace StringCalculatorKata.Tests
         [Fact]
         public void Return_0_when_the_input_string_is_empty()
         {
-            new StringCalculator().Add("").Should().Be(0);
+            StringCalculator.Add("").Should().Be(0);
         }
 
         [Fact]
         public void Return_number_value_when_the_input_string_contains_one_number()
         {
-            new StringCalculator().Add("3").Should().Be(3);
-            new StringCalculator().Add("1").Should().Be(1);
-            new StringCalculator().Add("42").Should().Be(42);
+            StringCalculator.Add("3").Should().Be(3);
+            StringCalculator.Add("1").Should().Be(1);
+            StringCalculator.Add("42").Should().Be(42);
         }
     }
 
-    public class StringCalculator
+    public static class StringCalculator
     {
-        public int Add(string numbers)
+        public static int Add(string numbers)
         {
             if (numbers.Length == 0)
                 return 0;
