e637aaae17658d90217ad5cb13cc8dd44eb5a09e
Pietro Di Bello <pierodibello@gmail.com>
2021-06-03 17:59:38 +0200

    move AT test to separate test class

--- StringCalculatorKata.Tests/StringCalculatorAppTest.cs
+++ StringCalculatorKata.Tests/StringCalculatorAppTest.cs
@@ -1,30 +1,10 @@
-using System;
-using System.IO;
 using Moq;
-using Newtonsoft.Json;
-using StringCalculatorKata.Terminal;
 using Xunit;
 
 namespace StringCalculatorKata.Tests
 {
     public class StringCalculatorAppTest
     {
-        [Fact]
-        public void Print_result_to_console_when_correctly_invoked()
-        {
-            var consoleSpy = new StringWriter();
-            Console.SetOut(consoleSpy);
-            Console.SetIn(new StringReader("1,2,3\n"));
-
-            var stringCalculator = new StringCalculator(new DummyLog(), new DummyNotifier());
-            var app = new StringCalculatorApp(stringCalculator, new ConsoleResultPrinter(), new ConsoleLineReader());
-
-            app.run();
-
-            Assert.Equal("The result is 6\n" +
-                         "another input please\n", consoleSpy.GetStringBuilder().ToString());
-        }
-
         [Fact]
         public void Output_the_result_to_a_result_printer()
         {
