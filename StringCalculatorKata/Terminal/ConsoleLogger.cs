b0ff00b2e553a8ecd4581f01792a80fcd8f4714d
Pietro Di Bello <pierodibello@gmail.com>
2021-06-03 17:49:36 +0200

    add a nicer logger

--- StringCalculatorKata/Terminal/ConsoleLogger.cs
+++ StringCalculatorKata/Terminal/ConsoleLogger.cs
@@ -4,6 +4,17 @@ namespace StringCalculatorKata.Terminal
 {
     public class ConsoleLogger : ILogger
     {
-        public void Write(string text) => Console.Out.WriteLine("[logger] {0}", text);
+        public void Write(string text)
+        {
+            WithGreyColor(() => Console.Out.WriteLine("[logger] {0}", text));
+        }
+
+        private static void WithGreyColor(Action consoleAction)
+        {
+            var previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            consoleAction.Invoke();
+            Console.ForegroundColor = previousColor;
+        }
     }
 }
