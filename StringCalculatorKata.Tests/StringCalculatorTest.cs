1094ad145576373bc35451a74417eb0431d5f653
ironzani <ivan.ronzani@aptos.com>
2021-06-03 11:08:17 +0200

    Created dummy logger and tests now passed

--- StringCalculatorKata.Tests/StringCalculatorTest.cs
+++ StringCalculatorKata.Tests/StringCalculatorTest.cs
@@ -1,90 +1,98 @@
-using System;
 using FluentAssertions;
 using Moq;
+using System;
 using Xunit;
 
 namespace StringCalculatorKata.Tests
 {
-    public class StringCalculatorTest
-    {
-        [Theory]
-        [InlineData("", 0)]
-        [InlineData(null, 0)]
-        [InlineData("   ", 0)]
-        public void Return_0_when_the_string_is_empty(string stringOfNumbers, int expectedSum)
-        {
-            CheckAdd(stringOfNumbers, expectedSum);
-        }
+	public class StringCalculatorTest
+	{
+		[Theory]
+		[InlineData("", 0)]
+		[InlineData(null, 0)]
+		[InlineData("   ", 0)]
+		public void Return_0_when_the_string_is_empty(string stringOfNumbers, int expectedSum)
+		{
+			CheckAdd(stringOfNumbers, expectedSum);
+		}
 
-        [Theory]
-        [InlineData("1", 1)]
-        [InlineData("2", 2)]
-        [InlineData("42", 42)]
-        public void Return_the_value_of_the_number_when_the_string_contains_just_that_number(string stringOfNumbers,
-            int expectedSum)
-        {
-            CheckAdd(stringOfNumbers, expectedSum);
-        }
+		[Theory]
+		[InlineData("1", 1)]
+		[InlineData("2", 2)]
+		[InlineData("42", 42)]
+		public void Return_the_value_of_the_number_when_the_string_contains_just_that_number(string stringOfNumbers,
+				int expectedSum)
+		{
+			CheckAdd(stringOfNumbers, expectedSum);
+		}
 
-        [Theory]
-        [InlineData("1,2", 3)]
-        [InlineData("1,1", 2)]
-        [InlineData("42,2", 44)]
-        public void Return_the_sum_of_the_two_comma_separated_numbers_in_the_string(string stringOfNumbers,
-            int expectedSum)
-        {
-            CheckAdd(stringOfNumbers, expectedSum);
-        }
+		[Theory]
+		[InlineData("1,2", 3)]
+		[InlineData("1,1", 2)]
+		[InlineData("42,2", 44)]
+		public void Return_the_sum_of_the_two_comma_separated_numbers_in_the_string(string stringOfNumbers,
+				int expectedSum)
+		{
+			CheckAdd(stringOfNumbers, expectedSum);
+		}
 
-        [Fact]
-        public void Handle_an_unknown_amount_of_numbers()
-        {
-            CheckAdd("1,2,3", 6);
-        }
+		[Fact]
+		public void Handle_an_unknown_amount_of_numbers()
+		{
+			CheckAdd("1,2,3", 6);
+		}
 
-        [Fact]
-        public void Handle_new_lines_between_numbers_instead_of_commas()
-        {
-            CheckAdd("1\n2,3", 6);
-        }
+		[Fact]
+		public void Handle_new_lines_between_numbers_instead_of_commas()
+		{
+			CheckAdd("1\n2,3", 6);
+		}
 
-        [Theory]
-        [InlineData("//;\n1;2", 3)]
-        [InlineData("//|\n1|2|3", 6)]
-        [InlineData("//$\n1$2$3$4", 10)]
-        [InlineData("//$$\n1$$2$$3$$4", 10)]
-        public void Support_a_custom_delimiter_specified_in_a_prefix_section(string stringOfNumbers, int expectedSum)
-        {
-            CheckAdd(stringOfNumbers, expectedSum);
-        }
+		[Theory]
+		[InlineData("//;\n1;2", 3)]
+		[InlineData("//|\n1|2|3", 6)]
+		[InlineData("//$\n1$2$3$4", 10)]
+		[InlineData("//$$\n1$$2$$3$$4", 10)]
+		public void Support_a_custom_delimiter_specified_in_a_prefix_section(string stringOfNumbers, int expectedSum)
+		{
+			CheckAdd(stringOfNumbers, expectedSum);
+		}
 
-        [Fact]
-        public void Negative_numbers_are_not_allowed()
-        {
-            var exception = Assert.Throws<Exception>(() => new StringCalculator().Add(("-1,1,-4")));
-            exception.Message.Should().Be("negatives not allowed: -1,-4");
-        }
+		[Fact]
+		public void Negative_numbers_are_not_allowed()
+		{
+			var exception = Assert.Throws<Exception>(() => new StringCalculator(new DummyLogger()).Add(("-1,1,-4")));
+			exception.Message.Should().Be("negatives not allowed: -1,-4");
+		}
 
-        [Fact]
-        public void Ignore_numbers_greater_than_1000()
-        {
-            CheckAdd("1,2, 1000, 1001", 1003);
-        }
+		[Fact]
+		public void Ignore_numbers_greater_than_1000()
+		{
+			CheckAdd("1,2, 1000, 1001", 1003);
+		}
 
-        [Fact]
-        public void Logging_add_results()
-        {
-            var logger = new Mock<ILogger>();
-            StringCalculator sc = new StringCalculator(logger.Object);
+		[Fact]
+		public void Logging_add_results()
+		{
+			var logger = new Mock<ILogger>();
+			StringCalculator sc = new StringCalculator(logger.Object);
 
-            sc.Add("3,1");
+			sc.Add("3,1");
 
-            logger.Verify(l => l.Write("Add:3,1:4"), Times.Once);
-        }
+			logger.Verify(l => l.Write("Add:3,1:4"), Times.Once);
+		}
 
-        private static void CheckAdd(string stringOfNumbers, int expectedSum)
-        {
-            new StringCalculator().Add(stringOfNumbers).Should().Be(expectedSum);
-        }
-    }
+		private static void CheckAdd(string stringOfNumbers, int expectedSum)
+		{
+			ILogger dummyLogger = new DummyLogger();
+			new StringCalculator(dummyLogger).Add(stringOfNumbers).Should().Be(expectedSum);
+		}
+	}
+
+	internal class DummyLogger : ILogger
+	{
+		public void Write(string v)
+		{
+		}
+	}
 }
--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -4,87 +4,90 @@ using System.Linq;
 
 namespace StringCalculatorKata
 {
-    public class StringCalculator
-    {
-        private static readonly string[] Separators = {",", "\n"};
+	public class StringCalculator
+	{
+		private static readonly string[] Separators = { ",", "\n" };
 
-        public StringCalculator(ILogger @object)
-        {
-        }
+		private ILogger logger { get; set; }
 
-        public StringCalculator()
-        {
-        }
+		public StringCalculator(ILogger logger)
+		{
+			this.logger = logger;
+		}
 
-        public int Add(string inputString)
-        {
-            if (inputString.IsEmpty())
-                return 0;
 
-            if (inputString.HasCustomDelimiter())
-            {
-                var delimiter = ExtractCustomDelimiterFrom(inputString);
-                var stringOfNumbers = ExtractStringOfNumbersFrom(inputString);
+		public int Add(string inputString)
+		{
+			if (inputString.IsEmpty())
+				return 0;
 
-                return Sum(stringOfNumbers, new[] {delimiter});
-            }
+			if (inputString.HasCustomDelimiter())
+			{
+				var delimiter = ExtractCustomDelimiterFrom(inputString);
+				var stringOfNumbers = ExtractStringOfNumbersFrom(inputString);
 
-            return Sum(inputString, Separators);
-        }
+				return Sum(stringOfNumbers, new[] { delimiter });
+			}
 
-        private static int Sum(string stringOfNumbers, string[] separators)
-        {
-            var numbers = ParseNumbers(stringOfNumbers, separators);
+			return Sum(inputString, Separators);
+		}
 
-            if (numbers.HasNegatives())
-                throw new Exception(BuildErrorMessageFor(numbers));
+		private int Sum(string stringOfNumbers, string[] separators)
+		{
+			var numbers = ParseNumbers(stringOfNumbers, separators);
 
-            return numbers.Where(LowerOrEqualTo1000).Sum();
-        }
+			if (numbers.HasNegatives())
+				throw new Exception(BuildErrorMessageFor(numbers));
 
-        private static string ExtractStringOfNumbersFrom(string inputString)
-        {
-            return inputString.Split('\n', 2)[1];
-        }
+			int sum = numbers.Where(LowerOrEqualTo1000).Sum();
 
-        private static string ExtractCustomDelimiterFrom(string stringOfNumbers)
-        {
-            return stringOfNumbers.Split('\n', 2)[0].Substring(2);
-        }
+			this.logger.Write(String.Format("Add:{0}:{1}", stringOfNumbers, sum));
+			return sum;
+		}
 
-        private static string BuildErrorMessageFor(List<int> numbers)
-        {
-            var negativeNumbers = numbers.FindAll(IsNegative());
-            var errorMessage = "negatives not allowed: " + string.Join(',', negativeNumbers);
-            return errorMessage;
-        }
+		private static string ExtractStringOfNumbersFrom(string inputString)
+		{
+			return inputString.Split('\n', 2)[1];
+		}
 
-        private static List<int> ParseNumbers(string stringOfNumbers, string[] separators)
-        {
-            return stringOfNumbers
-                .Split(separators, StringSplitOptions.None)
-                .Select(int.Parse).ToList();
-        }
+		private static string ExtractCustomDelimiterFrom(string stringOfNumbers)
+		{
+			return stringOfNumbers.Split('\n', 2)[0].Substring(2);
+		}
 
-        private static Predicate<int> IsNegative()
-        {
-            return number => number < 0;
-        }
+		private static string BuildErrorMessageFor(List<int> numbers)
+		{
+			var negativeNumbers = numbers.FindAll(IsNegative());
+			var errorMessage = "negatives not allowed: " + string.Join(',', negativeNumbers);
+			return errorMessage;
+		}
 
-        private static bool LowerOrEqualTo1000(int number)
-        {
-            return number <= 1000;
-        }
-    }
+		private static List<int> ParseNumbers(string stringOfNumbers, string[] separators)
+		{
+			return stringOfNumbers
+					.Split(separators, StringSplitOptions.None)
+					.Select(int.Parse).ToList();
+		}
 
-    internal static class Extensions
-    {
-        internal static bool HasCustomDelimiter(this string stringOfNumbers) => stringOfNumbers.StartsWith("//");
+		private static Predicate<int> IsNegative()
+		{
+			return number => number < 0;
+		}
 
-        internal static bool HasNegatives(this List<int> numbers) => numbers.Exists(IsNegative);
+		private static bool LowerOrEqualTo1000(int number)
+		{
+			return number <= 1000;
+		}
+	}
 
-        internal static bool IsEmpty(this string aString) => string.IsNullOrWhiteSpace(aString);
+	internal static class Extensions
+	{
+		internal static bool HasCustomDelimiter(this string stringOfNumbers) => stringOfNumbers.StartsWith("//");
 
-        private static bool IsNegative(this int number) => number < 0;
-    }
+		internal static bool HasNegatives(this List<int> numbers) => numbers.Exists(IsNegative);
+
+		internal static bool IsEmpty(this string aString) => string.IsNullOrWhiteSpace(aString);
+
+		private static bool IsNegative(this int number) => number < 0;
+	}
 }
